51f5df232a03bc3e1db8a56b449cf324e33184f9
George <george_whiting@hotmail.com>
2018-01-16 12:34:20 +0000

    adds a whole bunch of feature tests for restaurant crud operations and extensive button checking

--- spec/feature_tests/rating_spec.rb
+++ spec/feature_tests/rating_spec.rb
@@ -1,11 +1,43 @@
 feature 'Reviews' do
-  scenario "a user can rate..." do
+  scenario "a user can rate 5 stars..." do
     visit '/ratings/new'
     find('#5').click
-    fill_in('rating[review]', with: "wow nice")
+    fill_in('rating[review]', with: "wow real nice")
     click_button('Save Rating')
     expect(page).to have_content('Rating: 5')
+    expect(page).to have_content('Review: wow real nice')
+  end
+  scenario "a user can rate 4 stars..." do
+    visit '/ratings/new'
+    find('#4').click
+    fill_in('rating[review]', with: "wow nice")
+    click_button('Save Rating')
+    expect(page).to have_content('Rating: 4')
     expect(page).to have_content('Review: wow nice')
+  end
+  scenario "a user can rate 3 stars..." do
+    visit '/ratings/new'
+    find('#3').click
+    fill_in('rating[review]', with: "wow kinda nice")
+    click_button('Save Rating')
+    expect(page).to have_content('Rating: 3')
+    expect(page).to have_content('Review: wow kinda nice')
+  end
+  scenario "a user can rate 2 stars..." do
+    visit '/ratings/new'
+    find('#2').click
+    fill_in('rating[review]', with: "wow not nice")
+    click_button('Save Rating')
+    expect(page).to have_content('Rating: 2')
+    expect(page).to have_content('Review: wow not nice')
+  end
+  scenario "a user can rate 1 stars..." do
+    visit '/ratings/new'
+    find('#1').click
+    fill_in('rating[review]', with: "wow really not nice")
+    click_button('Save Rating')
+    expect(page).to have_content('Rating: 1')
+    expect(page).to have_content('Review: wow really not nice')
   end
 
 end
---
+++ spec/feature_tests/restaurant_crud_spec.rb
@@ -0,0 +1,36 @@
+feature 'Restaurant creation' do
+  scenario 'A user can add a new restaurant' do
+    visit '/restaurants/new'
+    fill_in 'restaurant[name]', with: "Test name"
+    fill_in 'restaurant[location]', with: "Test location"
+    fill_in 'restaurant[desc]', with: "Test description"
+    click_on 'Save Restaurant'
+    expect(page).to have_content "Test location"
+  end
+end
+
+feature 'Restaurant deletion' do
+  scenario 'A user can delete an existing restaurant' do
+    visit '/restaurants/new'
+    fill_in 'restaurant[name]', with: "Test name"
+    fill_in 'restaurant[location]', with: "Test location"
+    fill_in 'restaurant[desc]', with: "Test description"
+    click_on 'Save Restaurant'
+    visit '/restaurants'
+    click_link('Destroy')
+    expect(page).not_to have_content "Test location"
+  end
+end
+
+feature 'Restaurant edit' do
+  scenario 'A user can edit the details of a restaurant' do
+    visit '/restaurants/new'
+    fill_in 'restaurant[name]', with: "Test name"
+    fill_in 'restaurant[location]', with: "Test location"
+    fill_in 'restaurant[desc]', with: "Test description"
+    click_on 'Save Restaurant'
+    click_link('Edit')
+    fill_in 'restaurant[name]', with: "A different test name"
+    expect(page).to have_content "A different test name" 
+  end
+end
