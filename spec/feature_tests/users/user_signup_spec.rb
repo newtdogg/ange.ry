78d1a4d3883d8f0ebf58b99250b2fb33826e9aeb
newtdogg <edwardfrancisnewton@live.com>
2018-01-16 16:17:07 +0000

    added first test for users

---
+++ spec/feature_tests/users/user_signup_spec.rb
@@ -0,0 +1,11 @@
+feature "Users" do
+  scenario "a user can sign up to review" do
+    visit('/users/new')
+    fill_in('user[username]', with: "angery_man")
+    fill_in('user[email]'), with: "angery_man@aol.com")
+    fill_in('user[password]'), with: "12345")
+    fill_in('user[password_confirmation]'), with: "12345")
+    click_button('Signup')
+    expect(page).to have_content("Welcome anger_man")
+  end
+end
