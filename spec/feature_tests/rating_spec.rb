1904fcf5fecdb9cd6bfc6698786b84d243d0c3e6
George <george_whiting@hotmail.com>
2018-01-16 17:07:33 +0000

    edits tests to reflect updated urls

--- spec/feature_tests/rating_spec.rb
+++ spec/feature_tests/rating_spec.rb
@@ -1,6 +1,11 @@
 feature 'Reviews' do
+
+  before :each do
+    Restaurant.create(name: "name", location: "location", desc: "borger")
+  end
+
   scenario "a user can rate 5 stars..." do
-    visit '/ratings/new'
+    visit 'restaurants/1/ratings/new'
     find('#5').click
     fill_in('rating[review]', with: "wow real nice")
     click_button('Save Rating')
@@ -8,7 +13,7 @@ feature 'Reviews' do
     expect(page).to have_content('Review: wow real nice')
   end
   scenario "a user can rate 4 stars..." do
-    visit '/ratings/new'
+    visit 'restaurants/1/ratings/new'
     find('#4').click
     fill_in('rating[review]', with: "wow nice")
     click_button('Save Rating')
@@ -16,7 +21,7 @@ feature 'Reviews' do
     expect(page).to have_content('Review: wow nice')
   end
   scenario "a user can rate 3 stars..." do
-    visit '/ratings/new'
+    visit 'restaurants/1/ratings/new'
     find('#3').click
     fill_in('rating[review]', with: "wow kinda nice")
     click_button('Save Rating')
@@ -24,7 +29,7 @@ feature 'Reviews' do
     expect(page).to have_content('Review: wow kinda nice')
   end
   scenario "a user can rate 2 stars..." do
-    visit '/ratings/new'
+    visit 'restaurants/1/ratings/new'
     find('#2').click
     fill_in('rating[review]', with: "wow not nice")
     click_button('Save Rating')
@@ -32,7 +37,7 @@ feature 'Reviews' do
     expect(page).to have_content('Review: wow not nice')
   end
   scenario "a user can rate 1 stars..." do
-    visit '/ratings/new'
+    visit 'restaurants/1/ratings/new'
     find('#1').click
     fill_in('rating[review]', with: "wow really not nice")
     click_button('Save Rating')
