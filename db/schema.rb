592441d75e57918d87b95e09bfbdf95770059c50
George <george_whiting@hotmail.com>
2018-01-17 15:12:49 +0000

    fixed the password digest issue

--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,7 @@ ActiveRecord::Schema.define(version: 20180116154240) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "encrypted_password"
-    t.string "salt"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
