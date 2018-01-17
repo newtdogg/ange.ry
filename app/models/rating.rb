d30a688d5bc157a2771c9f6d7732fe78ecabbc04
James Lizamore <jwlizamore@gmail.com>
2018-01-17 15:31:57 +0000

    Finished server validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,4 @@
 class Rating < ApplicationRecord
+  validates :stars, presence: true
   belongs_to :restaurant
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :email, presence: true
+
   has_secure_password validations: false
 end
