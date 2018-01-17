5fc9a992bac4c96facabc7d8a52f734452b095da
newtdogg <edwardfrancisnewton@live.com>
2018-01-17 11:05:45 +0000

    fixed the database

--- db/migrate/20180116154240_create_users.rb
+++ db/migrate/20180116154240_create_users.rb
@@ -3,8 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.string :encrypted_password
-      t.string :salt
+      t.string :password
 
       t.timestamps
     end
--- spec/feature_tests/users/user_signup_spec.rb
+++ spec/feature_tests/users/user_signup_spec.rb
@@ -3,7 +3,7 @@ feature "Users" do
     visit('/users/new')
     fill_in('user[username]', with: "angery_man")
     fill_in('user[email]', with: "angery_man@aol.com")
-    fill_in('user[encrypted_password]', with: "12345")
+    fill_in('user[password]', with: "12345")
     fill_in('user[password_confirmation]', with: "12345")
     click_button('Signup')
     expect(page).to have_content("Welcome angery_man")
