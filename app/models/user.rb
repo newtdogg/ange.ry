d1854c778b8587e0c31f434467880bd5849f43d0
James Lizamore <jwlizamore@gmail.com>
2018-01-17 16:28:01 +0000

    User email and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: {with: /@/}
 
   has_secure_password
 end
