4b7fe162f6a68d3d3749ac21fe2fcfedf6a97b31
newtdogg <edwardfrancisnewton@live.com>
2018-01-18 12:09:49 +0000

    added tests for user to view their ratings, broken other tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   #resources :ratings
-  resources :users  
+  resources :users do
+    resources :ratings
+  end
   resources :restaurants do
     resources :ratings
   end
