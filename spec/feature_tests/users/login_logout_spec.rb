5b6c2da6bddfd919f78464b9d3e5f63216f966e3
newtdogg <edwardfrancisnewton@live.com>
2018-01-18 10:32:55 +0000

    added further css and fixed test

--- spec/feature_tests/users/login_logout_spec.rb
+++ spec/feature_tests/users/login_logout_spec.rb
@@ -1,6 +1,6 @@
 feature "Users" do
   scenario "after signing up a user can log out" do
-    sign_up("angery_man", "angery_man@aol.com", "12345", "12345")
+    sign_up
     click_button("Log Out")
     expect(page).to_not have_content("Welcome angery_man")
   end
