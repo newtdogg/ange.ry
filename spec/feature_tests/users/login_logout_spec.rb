104b7ac8f123b9a90512de98eaefef949fd1d72e
James Lizamore <jwlizamore@gmail.com>
2018-01-18 15:52:26 +0000

    Fixed tests, added login test

--- spec/feature_tests/users/login_logout_spec.rb
+++ spec/feature_tests/users/login_logout_spec.rb
@@ -1,8 +1,15 @@
 feature "Users" do
   scenario "after signing up a user can log out" do
     sign_up
-    click_button("Log Out")
-    expect(page).to_not have_content("Welcome angery_man")
+    click_on("Log out")
+    expect(page).to_not have_content("angery_man")
   end
 
+  scenario "a user can login" do
+    sign_up
+    click_on('Log out')
+    click_on('Log in')
+    sign_in
+    expect(page).to have_content("angery_man")
+  end
 end
