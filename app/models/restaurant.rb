7e095727b8bcd3609d6ac5dea336de5a9741c393
George <george_whiting@hotmail.com>
2018-01-18 12:03:34 +0000

    moves calculate average method from index.erb to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,11 @@ class Restaurant < ApplicationRecord
   validates :desc, presence: true
   # Destroys all related ratings on deletion
   has_many :ratings, dependent: :destroy
+
+  def calculate_average_rating(id)
+    restaurant = Restaurant.find(id)
+    restaurant.ratings.count == 0 ? "No ratings yet!" : ((restaurant.ratings.inject(0){|sum, i| sum + i.stars} / (restaurant.ratings.count).to_f).round(1))
+  end
+
+
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-# RSpec.describe Restaurant, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe Rating do
+  describe '#calculate_average_rating' do
+    it 'should calculate the average rating of a restaurant' do
+    restaurant = Restaurant.create(name: "name", location: "location", desc: "desc")
+      Rating.create(stars: 3, review: "decent", restaurant_id: 1)
+      Rating.create(stars: 5, review: "nice", restaurant_id: 1)
+      expect(restaurant.calculate_average_rating(1)).to eq 4
+    end
+  end
+end
